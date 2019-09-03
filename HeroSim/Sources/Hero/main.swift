9d6f58a01f0015d964789c874a8f89757140002a
Quentin Zeller <quentin@zeller.xyz>
2019-09-03 14:30:06 +0200

    change sources

--- HeroSim/Sources/Hero/main.swift
+++ HeroSim/Sources/Hero/main.swift
@@ -23,15 +23,15 @@ let labelExecution = {d, l in LabelTools.dynamicReplace(t: d, label: l, interpre
 
 let p = PetriNet()
 p.loadDefinitionFile(path: fileDef, labelExecution: labelExecution)
-//p.manualRun(transitionName: "t1", binding: ["a":"2","c":"sub"])
+p.manualRun(transitionName: "t1", binding: ["a":"2","c":"sub"])
 //p.randomRun()
 //p.transitions["t2"]?.fire()
 
 
-let allMarking = p.marking()
-
-print("________________________________________________")
-
-for i in allMarking{
-    print("🔷", i.sorted(by: { $0.0 < $1.0 }))}
-print("🔷🔷🔷 All different marking : ", allMarking.count)
+//let allMarking = p.marking()
+//
+//print("________________________________________________")
+//
+//for i in allMarking{
+//    print("🔷", i.sorted(by: { $0.0 < $1.0 }))}
+//print("🔷🔷🔷 All different marking : ", allMarking.count)
